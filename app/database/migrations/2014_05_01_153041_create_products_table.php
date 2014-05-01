0c3904cbc0f992ba19afd1b786bd9a2e9d91ed99
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-05-01 15:33:33 -0300

    Criação da Tabela de Produtos

---
+++ app/database/migrations/2014_05_01_153041_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('products', function($table){
+			$table->increments('id');
+			$table->stirng('name');
+			$table->decimal('price',5,2);
+			$table->text('description');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
