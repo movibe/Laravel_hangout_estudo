4a7ba9add63d73690fc5147e811dcc386a9190cf
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-05-01 16:03:10 -0300

    Criado um Template e chamando ele pelo Controller

---
+++ app/models/Product.php
@@ -0,0 +1,8 @@
+<?php
+
+// Model Criado com base na Tabela products
+class Product extends Eloquent{
+
+	// Selecionando a Tabela
+	protected $table = 'products';
+}
---
+++ app/views/layout/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Hangout Laravel</title>
+	<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+<body>
+	<div id="header">
+		<h1 class="logo">Larashop</h1>
+	</div>
+	<div id="main">
+		{{ $content }}
+	</div>
+</body>
+</html>
